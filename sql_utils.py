a0b402a54194e43a7239aa657b48e036b797a2b1
zoonomen-APP <appeterson37@gmail.com>
2025-06-27 14:26:23 -0700

    Adds in check for if the NE data exists

--- sql_utils.py
+++ sql_utils.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import sqlite3
 import os
-
+from pathlib import Path
 def load_to_db(out_dl_tsv, raw_dir):
     df=pd.read_csv(out_dl_tsv, sep="\t")
     db_name = os.path.basename(os.path.dirname(os.path.dirname(raw_dir)))
     db_path = f"C:/Lichen/SQL/{db_name}.db"
+    print(f'Attempting to write db to {db_path}')
     conn=sqlite3.connect(db_path)
     df.to_sql("full",conn,index=False,if_exists="replace")
+    print("Attempting to add in NE dataset")
+    ne_path = Path("c:/Lichen/datastor/NE/MA/sb/NEdf")
+    if ne_path.exists():
+        ne_df=pd.read_csv(ne_path,sep="|")
+        ne_df.to_sql("New_England",conn,index=False,if_exists="replace")
+    
     conn.close()
-
-# import dirs
-
-# run_no = 76
-# raw_dir = dirs.raw_dir(run_no)
-# output_fn = 'full_trans'
-# tsv_dir = dirs.tsv_dir(run_no)
-# out_dl_tsv = f'{tsv_dir}{output_fn}.tsv'
-
-# load_to_db(out_dl_tsv, raw_dir)
